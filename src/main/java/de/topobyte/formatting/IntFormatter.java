2681c8527edd374bf30779699c145f3fe95bb55e
Sebastian Kürten <sebastian@topobyte.de>
2017-11-08 00:12:31 +0100

    Formatter for ints

---
+++ src/main/java/de/topobyte/formatting/IntFormatter.java
@@ -0,0 +1,40 @@
+// Copyright 2017 Sebastian Kuerten
+//
+// This file is part of simple-formatting.
+//
+// simple-formatting is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// simple-formatting is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with simple-formatting. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.formatting;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IntFormatter
+{
+
+	final static Logger logger = LoggerFactory.getLogger(IntFormatter.class);
+
+	public String format(int n)
+	{
+		StringBuilder buffer = new StringBuilder();
+		format(buffer, n);
+		return buffer.toString();
+	}
+
+	public void format(StringBuilder buffer, int n)
+	{
+		buffer.append(n);
+	}
+
+}
